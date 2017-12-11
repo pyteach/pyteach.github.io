2045091b941e1ae20b12e059775ee71ca03e6d18
wzsx <pytea@outlook.com>
2017-12-11 14:38:46 +0800

    blog

--- Js/MenuCookie.js
+++
@@ -1,8 +0,0 @@
-﻿function ShowMenu() {
-    var div = document.getElementById("MenuDiv");
-    div.style.display = "block";
-}
-function HideMenu() {
-    var div = document.getElementById("MenuDiv");
-    div.style.display = "none";
-}
